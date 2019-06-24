a5d9a5728b32e8551bba92821fc09731b3d78fac
Travis Hahn <tjxhahn@gmail.com>
2019-06-23 22:59:11 -0400

    Add files via upload

---
+++ Test/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TjWeb;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace TjWebTest
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Server TestServer = new Server("http://localhost", 8080);
+
+            TestServer.SetMaxThreads(100);
+
+            TestServer.AddRoute(RouteType.GET, "/", (HttpListenerRequest req, ResponseObject res) =>
+            {
+                res.SetStatus(200, "OK");
+                res.SetHeader("Content-Type", "text/html");
+
+                res.AddText("<h1>Just a Test, Man</h1>");
+                res.Send();
+            });
+
+            TestServer.Start();
+        }
+    }
+}
